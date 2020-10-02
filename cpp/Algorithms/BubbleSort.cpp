d8691e30870a03910270b995fc2367e8c01cb6d0
Purbasa Kar <38216228+PurvaKar@users.noreply.github.com>
2020-10-02 22:16:52 +0530

    Update BubbleSort.cpp

--- cpp/Algorithms/BubbleSort.cpp
+++ cpp/Algorithms/BubbleSort.cpp
@@ -14,7 +14,7 @@ int main()
             if (arr[j] > arr[j+1])
                 swap(arr[j], arr[j+1]);
     cout<<"Sorted Array is:";
-    for(i=0;i<n-1;i++)
+    for(i=0;i<n;i++)
         cout<<arr[i]<<" ";
     return 0;
 }
