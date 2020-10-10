67ab493333aba9a0d35e1901528b5211e16b1187
harsh-surya <harsh_s@bt.iitr.ac.in>
2020-10-11 01:35:21 +0530

    Added two pointer algorithm for java

---
+++ java/Algorithms/TwoPointersAlgo.java
@@ -0,0 +1,46 @@
+package twoPointerAlgorithm;
+
+import java.util.*;
+
+public class ArrayTripletSumBy2PointersAlgo {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] a= new int[5];
+		boolean sum = false;
+		for(int i=0; i<5; i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		Arrays.sort(a);
+		for(int i=0 ; i<3 ; i++)
+		{
+			if(TwoSum(a, -a[i], i+1))
+			{
+				sum = true;
+			}
+		}
+		System.out.println(sum);
+	}
+
+	public static boolean TwoSum(int a[], int x, int i)
+	{
+		int j = a.length-1;
+		while(i<j)
+		{
+			if(a[i]+a[j]<x)
+			{
+				i++;
+			}
+			else if(a[i]+a[j]>x)
+			{
+				j--;
+			}
+			else 
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+}
