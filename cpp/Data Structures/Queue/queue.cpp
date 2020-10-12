6b536092287b4c50ca4afb8c438ba0e816fc17c1
Manan Jain <manansj01@gmail.com>
2020-10-12 16:22:48 +0530

    Added linked queue and stack and BST

--- cpp/Data Structures/Queue/queue.cpp
+++ cpp/Data Structures/Queue/queue.cpp
@@ -1,82 +1,102 @@
-#include "queue.h"
-using namespace std;
+#include<stdio.h>
+#include<process.h>
 
-//Constructor por defecto
-template <typename T>
-Queue<T>::Queue()
-{
-    m_head = NULL;
-    m_last = NULL;
-}
-//Insertar
-template <typename T>
-void Queue<T>::add_end(T data_)
-{
-    Node<T> *new_node = new Node<T>(data_);
-    if (!m_head)
-    {
-        m_head = new_node;
-    }
-    else
-    {
-        m_last->next = new_node;
-    }
-    m_last = new_node;
-}
+void enqueue(int[],int);
+void dequeue(int[]);
+void display_Queue(int[],int,int);
 
-//Obtener Nodo
-template <typename T>
-void Queue<T>::getInfoFirst()
-{
-    if (!m_head)
-    {
-        cout << "The queue is empty" << endl;
-    }
-    else
-    {
-        cout << m_head->print();
-        cout << endl
-             << endl;
-    }
-}
+int Queue[60],size,front=-1,rear=-1;
 
-//Eliminar nodos
-template <typename T>
-void Queue<T>::dell()
-{
-    if (!m_head)
-    {
-        cout << "The queue is empty" << endl;
-    }
-    else
-    {
-        Node<T> *temp = m_head;
-        m_head = m_head->next;
-        temp->next = NULL;
-    }
-}
+int main()
+ {
+     int item,choice,ret;
+     printf("\n ENETR THE SIZE OF QUEUE :");
+     scanf("%d",&size);
+     printf("\n\n\t\t OPERATION MENU\n\n\t\t  1.ENQUEUE\n\t\t  2.DEQUEUE\n\t\t  3.DISPLAY\n\t\t  4.EXIT ");
+     do
+     {
+        printf("\n\n ENETR YOUR CHOICE : ");
+        scanf("%d",&choice);
+        switch(choice)
+          {
+            case 1:
+                {
+                    printf("\n ENTER THE ITEM :");
+                    scanf("%d",&item);
+                    enqueue(Queue,item);
+                    break;
+                }
+            case 2:
+                {
+                   dequeue(Queue);
+                   break;
+                }
+            case 3:
+                {
+                    display_Queue(Queue,front,rear);
+                    break;
+                }
+            case 4:
+            {
+                break;
+            }
+          }
+     }while(choice!=4);
+     return 0;
 
-//Imprimir cola
-template <typename T>
-void Queue<T>::print()
-{
-    Node<T> *temp = m_head;
-    if (!m_head)
-    {
-        cout << "The queue is empty " << endl;
-    }
-    else
-    {
-        while (temp)
+ }
+
+ void enqueue(int Queue[],int temp)
+  {
+      if(rear==size-1)
+       {
+           printf("\n QUEUE OVERFLOW");
+           return ;
+       }
+      else
+        if(rear==-1)
+          {
+              front=rear=0;
+              Queue[rear]=temp;
+          }
+        else
         {
-            temp->print();
-            if (!temp->next)
-                cout << "NULL";
-            temp = temp->next;
+            rear++;
+            Queue[rear]=temp;
         }
-    }
-    cout << endl << endl;
-}
+  }
 
-template<typename T>
-Queue<T>::~Queue(){}
+  void dequeue(int Queue[])
+   {
+       if(front==-1)
+        {
+            printf("\n QUEUE UNDERFLOW");
+            return ;
+        }
+        if(front==rear)
+        {
+            printf("\n THE DELETED ITEM IS  %d",Queue[front]);
+            front=-1;
+        }
+
+       else
+       {
+         printf("\n THE DELETED ITEM IS  %d",Queue[front]);
+         front++;
+       }
+  }
+
+ void display_Queue(int Queue[],int front,int rear)
+  {
+      if(front==-1)
+        printf("\n QUEUE UNDERFLOW");
+      else
+      {
+          printf("\n QUEUE IS :");
+          for(int i=front;i<rear;++i)
+          {
+           printf("%d<- ",Queue[i]);
+          }
+          printf("%d",Queue[rear]);
+      }
+  }
