6810188c07bb68b7b1951fba62cb6a517d8f5e3e
anshita2000 <44469617+anshita2000@users.noreply.github.com>
2021-10-04 11:14:59 +0530

    Update list.cpp

--- cpp/Data Structures/LinkedList/list.cpp
+++ cpp/Data Structures/LinkedList/list.cpp
@@ -163,6 +163,29 @@ void List<T>::del_by_position(int pos)
     }
 }
 
+//delete alternate nodes
+void deleteAlt(Node *head) 
+{ 
+    if (head == NULL) 
+        return; 
+  
+    /* Initialize prev and node to be deleted */
+    Node *prev = head; 
+    Node *node = head->next; 
+  
+    while (prev != NULL && node != NULL) 
+    { 
+        /* Change next link of previous node */
+        prev->next = node->next; 
+  
+        /* Update prev and node */
+        prev = prev->next; 
+        if (prev != NULL) 
+            node = prev->next; 
+    } 
+} 
+
+
 //Fill by data
 template <typename T>
 void List<T>::fill_by_user(int dim)
